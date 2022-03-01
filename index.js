6347ad15b5192b829c1f42e5cf608ffd80fda0ba
Javier Guerra <javier.j.guerra4@gmail.com>
2022-03-01 00:39:31 -0600

    When I added a second render it overwrote the first render

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-ReactDOM.render(<h1>Hello, everyone</h1>, document.getElementById("root"));
+ReactDOM.render(<h1>Second try</h1>, document.getElementById("root"));
+ReactDOM.render(
+  <p>This is a test paragraph</p>,
+  document.getElementById("root")
+);
