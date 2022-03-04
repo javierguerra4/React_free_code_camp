f2bbbc0837adaeb6caedd1b9aa27f72d4402b690
Javier Guerra <javier.j.guerra4@gmail.com>
2022-03-04 15:43:34 -0600

    Challenge, I forgot to add the return statement in my function, fixed and complete

--- index.js
+++ index.js
@@ -1,31 +1,25 @@
-/*
-Challenge: Starting from scratch, build and render the 
-HTML for our section project. Check the Google slide for 
-what you're trying to build.
+/**
+Challenge: 
 
-We'll be adding styling to it later.
+Part 1: Create a page of your own using a custom Page component
 
-Hints:
-* The React logo is a file in the project tree, so you can
-  access it by using `src="./react-logo.png" in your image
-  element
-* You can also set the `width` attribute of the image element
-  just like in HTML. In the slide, I have it set to 40px
+It should return an ordered list with the reasons why you're
+excited to be learning React :)
+
+Render your list to the page
+
  */
-const page = (
-  <div>
-    <nav>
-      <img src="images/react_logo.png" width="40px"></img>
-    </nav>
-    <h1>Fun facts about React</h1>
-    <ul>
-      <li>Was first released in 2013</li>
-      <li>Was originally created by Jordan Walke</li>
-      <li>Has well over 100k stars on Github</li>
-      <li>Is maintained by Facebook</li>
-      <li>Powers thousands of enterprise apps, including mobile apps</li>
-    </ul>
-  </div>
-);
 
-ReactDOM.render(page, document.getElementById("root"));
+function Page() {
+  return (
+    <div>
+      <ol>
+        <li>It's fun</li>
+        <li>It's hireable</li>
+        <li>I want to make cool websites</li>
+      </ol>
+    </div>
+  );
+}
+
+ReactDOM.render(<Page />, document.getElementById("root"));
