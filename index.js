18cd28e9dc0024d2501a54b154940d856cee34c8
Javier Guerra <javier.j.guerra4@gmail.com>
2022-03-03 19:01:09 -0600

    Finished challenge, navbar challenge

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
-// ReactDOM.render(<h1>Hello, React!</h1>, document.getElementById("root"))
+// const page = (
+//     <div>
+//         <h1 className="header">This is JSX</h1>
+//         <p>This is a paragraph</p>
+//     </div>
+// )
 
 /* 
-Challenge - recreate the above line of code in vanilla JS by creating and
-appending an h1 to our div#root (without using innerHTML).
+Challenge: 
 
-- Create a new h1 element
-- Give it some textContent
-- Give it a class name of "header"
-- append it as a child of the div#root
-    
+Create a navbar in JSX:
+    - Use the semantic `nav` element as the parent wrapper
+    - Have an h1 element with the brand name of your "website"
+    - Insert an unordered list for the other nav elements
+        - Inside the `ul`, have three `li`s for "Pricing",
+        "About", and "Contact"
+    - Don't worry about styling yet - it'll just be plain-looking HTML for now
 */
-
-var newH1 = document.createElement("h1");
-
-var newContent = document.createTextNode("Hello from Rick and Morty!");
-
-newH1.appendChild(newContent);
-
-var currentId = document.getElementById("root");
-document.body.insertBefore(newH1, currentId);
-
-// Instructor solution
-const h1 = document.createElement("h1");
-h1.textContent = "This is an imperative way to program";
-h1.className = "header";
-document.getElementById("root").append(h1);
+const navBar = (
+  <div>
+    <nav>
+      <h1>Website Name</h1>
+      <ul>
+        <li>Pricing</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  </div>
+);
+ReactDOM.render(navBar, document.getElementById("root"));
