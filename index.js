645ccf769eef8dd9382f01c8afa419268264d186
Javier Guerra <javier.j.guerra4@gmail.com>
2022-03-03 18:41:22 -0600

    Added instructor solution to vanilla js challenge

--- index.js
+++ index.js
@@ -19,3 +19,9 @@ newH1.appendChild(newContent);
 
 var currentId = document.getElementById("root");
 document.body.insertBefore(newH1, currentId);
+
+// Instructor solution
+const h1 = document.createElement("h1");
+h1.textContent = "This is an imperative way to program";
+h1.className = "header";
+document.getElementById("root").append(h1);
