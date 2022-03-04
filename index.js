2eb3bebb9f6e06f615369b991e00ce155dc929ed
Javier Guerra <javier.j.guerra4@gmail.com>
2022-03-03 20:41:02 -0600

    Finished challenge and created page

--- index.js
+++ index.js
@@ -14,8 +14,17 @@ Hints:
  */
 const page = (
   <div>
-    <nav src="images/react_logo.png"></nav>
+    <nav>
+      <img src="images/react_logo.png" width="40px"></img>
+    </nav>
     <h1>Fun facts about React</h1>
+    <ul>
+      <li>Was first released in 2013</li>
+      <li>Was originally created by Jordan Walke</li>
+      <li>Has well over 100k stars on Github</li>
+      <li>Is maintained by Facebook</li>
+      <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
   </div>
 );
 
