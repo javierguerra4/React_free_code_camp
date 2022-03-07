3991da896be62564a097c1226b3ea7527980c37d
Javier Guerra <javier.j.guerra4@gmail.com>
2022-03-07 15:38:43 -0600

    Completed part 2 of challenge

--- index.js
+++ index.js
@@ -10,14 +10,37 @@ Render your list to the page
 
  */
 
+/**
+Challenge: 
+
+Part 2: 
+- Add a `header` element with a nested `nav` element. Inside the `nav`,
+  include a `img` element with the image of the React logo inside
+  (src="./react-logo.png") and make sure to set the width to something
+  more manageable so it doesn't take up the whole screen
+- Add an `h1` with some text describing the page. (E.g. "Reasons
+  I'm excited to learn React"). Place it above the ordered list.
+- Add a `footer` after the list that says: 
+    "© 20xx <last name here> development. All rights reserved."
+ */
+
 function Page() {
   return (
     <div>
+      <header>
+        <nav>
+          <img src="images/react_logo.png" width="40px;"></img>
+        </nav>
+      </header>
+      <h1>Reasons I'm excited to learn React!</h1>
       <ol>
         <li>It's fun</li>
         <li>It's hireable</li>
         <li>I want to make cool websites</li>
       </ol>
+      <footer>
+        <p>© 2022 Guerra development. All rights reserved.</p>
+      </footer>
     </div>
   );
 }
