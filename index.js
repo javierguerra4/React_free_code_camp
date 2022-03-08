412469527ce3d5ba863d75ed22c76d24c0bc6e55
Javier Guerra <javier.j.guerra4@gmail.com>
2022-03-08 13:54:52 -0600

    Problem fixed: added a div to MainContent

--- index.js
+++ index.js
@@ -8,19 +8,25 @@ Challenge:
 */
 function Header() {
   return (
-    <>
-      <header>
-        <nav>
-          <img src="images/react_logo.png" width="40px" />
-        </nav>
-      </header>
-    </>
+    <header>
+      <nav>
+        <img src="images/react_logo.png" width="40px" />
+      </nav>
+    </header>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <small>© 2021 Ziroll development. All rights reserved.</small>
+    </footer>
   );
 }
 
 function MainContent() {
   return (
-    <>
+    <div>
       <h1>Reasons I'm excited to learn React</h1>
       <ol>
         <li>
@@ -28,26 +34,17 @@ function MainContent() {
         </li>
         <li>I'm more likely to get a job as a developer if I know React</li>
       </ol>
-    </>
-  );
-}
-function Footer() {
-  return (
-    <>
-      <footer>
-        <small>© 2021 Ziroll development. All rights reserved.</small>
-      </footer>
-    </>
+    </div>
   );
 }
 
 function Page() {
   return (
-    <>
+    <div>
       <Header />
       <MainContent />
       <Footer />
-    </>
+    </div>
   );
 }
 
