ea53b1ab920f0ed434046057851e8379cdd4daec
Javier Guerra <javier.j.guerra4@gmail.com>
2022-03-08 12:25:37 -0600

    Completed mini challenge

--- index.js
+++ index.js
@@ -1,45 +1,28 @@
 /**
-Challenge: 
-
-Part 1: Create a page of your own using a custom Page component
-
-It should return an ordered list with the reasons why you're
-excited to be learning React :)
-
-Render your list to the page
-
- */
-
-/**
-Challenge: 
-
-Part 2: 
-- Add a `header` element with a nested `nav` element. Inside the `nav`,
-  include a `img` element with the image of the React logo inside
-  (src="./react-logo.png") and make sure to set the width to something
-  more manageable so it doesn't take up the whole screen
-- Add an `h1` with some text describing the page. (E.g. "Reasons
-  I'm excited to learn React"). Place it above the ordered list.
-- Add a `footer` after the list that says: 
-    "© 20xx <last name here> development. All rights reserved."
- */
+Mini Challenge:
+Move the `header` element from Page into 
+its own component called "Header"
+*/
+function Header() {
+  <header>
+    <nav>
+      <img src="images/react_logo.png" width="40px" />
+    </nav>
+  </header>;
+}
 
 function Page() {
   return (
     <div>
-      <header>
-        <nav>
-          <img src="images/react_logo.png" width="40px;"></img>
-        </nav>
-      </header>
-      <h1>Reasons I'm excited to learn React!</h1>
+      <h1>Reasons I'm excited to learn React</h1>
       <ol>
-        <li>It's fun</li>
-        <li>It's hireable</li>
-        <li>I want to make cool websites</li>
+        <li>
+          It's a popular library, so I'll be able to fit in with the cool kids!
+        </li>
+        <li>I'm more likely to get a job as a developer if I know React</li>
       </ol>
       <footer>
-        <small>© 2022 Guerra development. All rights reserved.</small>
+        <small>© 2021 Ziroll development. All rights reserved.</small>
       </footer>
     </div>
   );
